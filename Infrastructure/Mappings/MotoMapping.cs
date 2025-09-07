b74289c02d4b1e724576cc5de996854cc1b51324
mbarros46 <miguelbarrosramos46@gmail.com>
2025-09-06 23:11:45 -0300

    chore(infrastructure): padronizar EntityTypeConfiguration e namespaces

--- Infrastructure/Mappings/MotoMapping.cs
+++ Infrastructure/Mappings/MotoMapping.cs
@@ -1,39 +1,35 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using MottuCrudAPI.Persistense;
+using Domain.Entities;
 
+namespace Infrastructure.EF.Config;
 
-
-namespace MottuCrudAPI.Infrastructure.Mappings
+public class MotoMapping : IEntityTypeConfiguration<Moto>
 {
-    public class MotoMapping : IEntityTypeConfiguration<Moto>
+    public void Configure(EntityTypeBuilder<Moto> builder)
     {
-        public void Configure(EntityTypeBuilder<Moto> builder)
-        {
-            builder.ToTable("MOTO");
+        builder.ToTable("MOTO");
 
-            builder.HasKey(m => m.Id);
+        builder.HasKey(m => m.Id);
 
-            builder.Property(m => m.Placa)
-                .IsRequired()
-                .HasMaxLength(7) 
-                .IsFixedLength();
+        builder.Property(m => m.Placa)
+            .IsRequired()
+            .HasMaxLength(7)
+            .IsFixedLength();
 
-            builder.Property(m => m.Ano)
-                .IsRequired();
+        builder.Property(m => m.Ano)
+            .IsRequired();
 
-            builder.Property(m => m.Modelo)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(m => m.Modelo)
+            .IsRequired()
+            .HasMaxLength(100);
 
-            builder.Property(m => m.PatioId)
-                .IsRequired(false); 
+        builder.Property(m => m.PatioId)
+            .IsRequired(false);
 
-            
-            builder.Property(m => m.Status)
-                .HasMaxLength(20)
-                .IsRequired(false);
-        }
+        builder.Property(m => m.Status)
+            .HasMaxLength(20)
+            .IsRequired(false);
     }
 }
--- Infrastructure/Mappings/PatioMapping.cs
+++ Infrastructure/Mappings/PatioMapping.cs
@@ -1,30 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using MottuCrudAPI.Persistense;
-
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
 
-namespace MottuCrudAPI.Infrastructure.Mappings
+namespace Infrastructure.EF.Config;
+
+public class PatioMapping : IEntityTypeConfiguration<Patio>
 {
-    public class PatioMapping: IEntityTypeConfiguration<Patio>
+    public void Configure(EntityTypeBuilder<Patio> builder)
     {
-        public void Configure(EntityTypeBuilder<Patio> builder)
-        {
-            builder.ToTable("PATIO");
+        builder.ToTable("PATIO");
 
-            builder.HasKey(p => p.Id);
+        builder.HasKey(p => p.Id);
 
-            builder.Property(p => p.Nome)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(p => p.Nome)
+            .IsRequired()
+            .HasMaxLength(100);
 
-            builder.Property(p => p.Endereco)
-                .IsRequired();
+        builder.Property(p => p.Endereco)
+            .IsRequired();
 
-            builder.Property(p => p.Capacidade)
-                .IsRequired();
+        builder.Property(p => p.Capacidade)
+            .IsRequired();
 
-            builder.Property(p => p.OcupacaoAtual)
-                .IsRequired();
-        }
+        builder.Property(p => p.OcupacaoAtual)
+            .IsRequired();
     }
 }
