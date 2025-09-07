75b203e55292726b9cf4233815f02c1f9e21e070
mbarros46 <miguelbarrosramos46@gmail.com>
2025-09-06 23:17:52 -0300

    style(clean-code): remover código morto, usar CancellationToken e AsNoTracking

--- Controllers/MotoController.cs
+++ Controllers/MotoController.cs
@@ -1,10 +1,6 @@
-using c_.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Application.DTOs;
 using Application.Services;
-using System.Net;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Numerics;
 
 
 namespace MottuCrudAPI.Controllers
