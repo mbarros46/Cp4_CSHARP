4101db991819738644d1765974bc1f7b7273278c
mbarros46 <miguelbarrosramos46@gmail.com>
2025-10-26 16:43:32 -0300

    docs: adicionar comentários XML em PatioController para melhorar Swagger

--- Controllers/PatioController.cs
+++ Controllers/PatioController.cs
@@ -14,11 +14,20 @@ namespace MottuCrudAPI.Controllers
         private readonly PatioService _svc;
         public PatioController(PatioService svc) => _svc = svc;
 
+        /// <summary>
+        /// Lista todos os pátios cadastrados.
+        /// </summary>
+        /// <param name="ct">Token de cancelamento</param>
         [HttpGet]
         [ProducesResponseType(typeof(List<PatioResponse>), StatusCodes.Status200OK)]
         public async Task<ActionResult<List<PatioResponse>>> Get(CancellationToken ct)
             => Ok(await _svc.ListAsync(ct));
 
+        /// <summary>
+        /// Obtém um pátio pelo identificador.
+        /// </summary>
+        /// <param name="id">Identificador do pátio</param>
+        /// <param name="ct">Token de cancelamento</param>
     [HttpGet("{id:guid}")]
     [ProducesResponseType(typeof(PatioResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -30,6 +39,11 @@ namespace MottuCrudAPI.Controllers
             return Ok(patio);
         }
 
+        /// <summary>
+        /// Cria um novo pátio.
+        /// </summary>
+        /// <param name="dto">Dados do pátio a ser criado</param>
+        /// <param name="ct">Token de cancelamento</param>
         [HttpPost]
         [ProducesResponseType(typeof(PatioResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -39,6 +53,12 @@ namespace MottuCrudAPI.Controllers
             return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
         }
 
+        /// <summary>
+        /// Atualiza os dados de um pátio existente.
+        /// </summary>
+        /// <param name="id">Identificador do pátio</param>
+        /// <param name="dto">Dados atualizados</param>
+        /// <param name="ct">Token de cancelamento</param>
         [HttpPut("{id:guid}")]
         [ProducesResponseType(typeof(PatioResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -49,6 +69,11 @@ namespace MottuCrudAPI.Controllers
             return Ok(updated);
         }
 
+        /// <summary>
+        /// Remove um pátio pelo identificador.
+        /// </summary>
+        /// <param name="id">Identificador do pátio</param>
+        /// <param name="ct">Token de cancelamento</param>
         [HttpDelete("{id:guid}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
